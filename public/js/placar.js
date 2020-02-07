44b5936c6a58e369125f383dead11e351b15c965
pedroegerland <pedro.egerland94@gmail.com>
2020-02-06 23:17:57 -0200

    Name Input and Results Table

---
+++ public/js/placar.js
@@ -0,0 +1,45 @@
+var usuario = $("#jogador");
+var name = '';
+
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+}
+
+usuario.on("input", function() {
+    name = usuario.val();
+});
+
+function novaLinha(usuario, palavras) {
+    usuario.on("input", function() {
+        name = usuario.val();
+    });
+    
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(name);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha() {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
